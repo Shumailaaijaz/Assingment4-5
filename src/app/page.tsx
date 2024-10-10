62fc8bcb6b366d4828d2468d6ddc4944922d835a
Shumailaaijaz <shooanaqshbandi@gmail.com>
2024-10-11 00:28:01 +0500

    removed unused link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import Image from "next/image";
+import Contact from "@/components/Contact";
 import About from "@/components/About";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import Contact from "@/components/Contact";
+
 
 
 export default function Home() {
