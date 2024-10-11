97af2e2a27ef8805735c8920cea8fd32410c449f
Shumailaaijaz <shooanaqshbandi@gmail.com>
2024-10-12 02:00:29 +0500

    update some unused code

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,21 +6,19 @@ import { BsTelephone } from "react-icons/bs";
 
 const Contact =() => {
     return(
-        <div id='Contact' className='pt-32 container'>
-          <div className='grid md:grid-cols-2 gap-10'>
-            <div className='space-y-8'>
-                <h2 className='text-9 font-bold'> Get in touch</h2>
-                <p className='text-gray-600 text-[20px] pt-2' >
-                Feel free to reach out if you'd like to discuss projects, opportunities, or just to chat about tech and development. I'm always open to connecting with like-minded individuals or anyone curious about my journey from homeopathy to IT. Looking forward to hearing from you!
-
-.
-                </p>
-                <div className='flex gap-3 items-center' >
+        <div id="Contact" className='pt-32 container'>
+            <div className='grid md:grid-cols-2 gap-10'>
+                <div className='space-y-8'>
+                  <h2 className='text-9 font-bold' data-aos="zoom-in-up"> Get in touch</h2>
+                    <p className='text-gray-600 text-[20px] pt-2' data-aos="zoom-in-up">
+                    Feel free to reach out if you'd like to discuss projects, opportunities, or just to chat about tech and development. I'm always open to connecting with like-minded individuals or anyone curious about my journey from homeopathy to IT. Looking forward to hearing from you!
+                    </p>
+                    <div className='flex gap-3 items-center' data-aos="zoom-in-up">
                     <CiMail size={30}/> shumailaaijaz359@gmail.com
-                </div>
-                <div className='flex gap-3 items-center' > 
+                    </div>
+                    <div className='flex gap-3 items-center' data-aos="zoom-in-up"> 
                     <BsTelephone size={30}/> (03333111567)
-                </div>
+                    </div>
                 </div>
                 <div className='space-y-9' >
                     <div className='flex flex-col gap-1'>
@@ -35,13 +33,13 @@ const Contact =() => {
                         className='h-[40px] bg-transparent border border-slate-600'
                         id='email'/>
                     </div>
-                      <div className='flex flex-col gap-1'>
+                    <div className='flex flex-col gap-1'>
                         <label htmlFor="msg">Message</label>
                         <textarea
                         className='bg-transparent border border-slate-600'
                         id='msg' rows={9}>
                         </textarea>
-                      </div>
+                    </div>
                     <button className='bg-slate-600 p-2 px-6' >Send</button>
                 </div>
             </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,61 +1,60 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
-import styles from './Projects.module.css'; 
+import styles from './Projects.module.css';
 
 const projects = [
   {
     id: 1,
     title: 'Blog Post 1',
-    image: '/todoApp.png',  // Ensure the image path is correct
-    description: 'This is the description for blog post 1.',
-    tags:["React" ,"Node", "CSS", "Typescript"],
+    image: '/todoApp.png',
+    description: 'This is a todo app for daily use.',
+    tags: ['React', 'Node', 'CSS', 'Typescript'],
   },
   {
     id: 2,
     title: 'Blog Post 2',
     image: '/calculator.png',
-    description: 'This is the description for blog post 2.',
-    tags:["Node", "Typescript"],
+    description: 'This is daily use calculator.',
+    tags: ['Node', 'Typescript'],
   },
   {
     id: 3,
     title: 'Blog Post 3',
     image: '/currencyconverter.png',
-    description: 'This is the description for blog post 3.',
-    tags:["HTML", "CSS", "Typescript"],
+    description: 'This is simple currency converter app.',
+    tags: ['HTML', 'CSS', 'Typescript'],
   },
   {
     id: 4,
     title: 'Blog Post 4',
     image: '/bankacApp.png',
-    description: 'This is the description for blog post 4.',
-    tags:["Node", "Typescript"],
+    description: 'This is simple banking app created using TS.',
+    tags: ['Node', 'Typescript'],
   },
   {
     id: 5,
     title: 'Blog Post 5',
     image: '/wordcounter.png',
-    description: 'This is the description for blog post 5.',
-    tags:["Node", "Typescript"],
+    description: 'This is the wordcounter App use to count words in a sentance.',
+    tags: ['Node', 'Typescript'],
   },
   {
     id: 6,
     title: 'Blog Post 6',
     image: '/appquiz.png',
-    description: 'This is the description for blog post 6.',
-    tags:["Node", "Typescript"],
+    description: 'This is the app using for exam preparation in quiz formate.',
+    tags: ['Node', 'Typescript'],
   },
 ];
 
 const Projects = () => {
   return (
-    <div id= 'projects' className={styles.container}>
+    <div id="projects" className={styles.container}>
       <h1 className={styles.title}>My Projects</h1>
       <div className={styles.projectsGrid}>
         {projects.map((project) => (
           <div key={project.id} className={styles.projectCard}>
-            {/* Image */}
             <div className={styles.imageContainer}>
               <Image
                 src={project.image}
@@ -65,20 +64,24 @@ const Projects = () => {
                 className={styles.projectImage}
               />
             </div>
-
-            {/* Description */}
             <div className={styles.projectDetails}>
               <h2>{project.title}</h2>
               <p>{project.description}</p>
-              <a className={styles.projectButton}>
-                Read More
-              </a>
+              <div className={styles.tags}>
+                {project.tags.map((tag, index) => (
+                  <span key={index} className={styles.tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <br />
+              <a className={styles.projectButton}>Read More</a>
             </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default Projects
+export default Projects;
